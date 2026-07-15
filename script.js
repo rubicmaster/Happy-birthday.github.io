cd636936d61d516b8cdc002b1bdeb863d2b00f71
rubicmaster <84274738+rubicmaster@users.noreply.github.com>
2026-07-15 16:50:10 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const cake = document.querySelector(".cake");
-const flame = document.querySelector(".flame");
-const message = document.getElementById("message");
-
-cake.addEventListener("click", () => {
-    flame.style.display = "none";
-    message.classList.add("show");
-});
